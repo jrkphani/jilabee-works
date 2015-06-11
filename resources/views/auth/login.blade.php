7a5e4ac2a2255c9adba0a14f44232518e663a2a3
ManiKandan Ram <manikandan@digitalchakra.in>
2015-06-11 08:35:51 +0530

    unwanted css removed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,8 +12,6 @@
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/sticky-footer-navbar.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/jotter.css') }}" rel="stylesheet">
-	@yield('css')
-
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
