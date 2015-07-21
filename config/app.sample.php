6eae1aaad3da114cd1d7a5303d5c817f2a79aee2
ManiKandan Ram <manikandan@digitalchakra.in>
2015-07-21 14:30:03 +0000

    sample config file

--- config/app.sample.php
+++ config/app.sample.php
@@ -4,64 +4,202 @@ return [
 
 	/*
 	|--------------------------------------------------------------------------
-	| Default Authentication Driver
+	| Application Debug Mode
 	|--------------------------------------------------------------------------
 	|
-	| This option controls the authentication driver that will be utilized.
-	| This driver manages the retrieval and authentication of the users
-	| attempting to get access to protected areas of your application.
-	|
-	| Supported: "database", "eloquent"
+	| When your application is in debug mode, detailed error messages with
+	| stack traces will be shown on every error that occurs within your
+	| application. If disabled, a simple generic error page is shown.
 	|
 	*/
 
-	'driver' => 'eloquent',
+	//'debug' => env('APP_DEBUG'),
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
-	| Authentication Model
+	| Application URL
 	|--------------------------------------------------------------------------
 	|
-	| When using the "Eloquent" authentication driver, we need to know which
-	| Eloquent model should be used to retrieve your users. Of course, it
-	| is often just the "User" model but you may use whatever you like.
+	| This URL is used by the console to properly generate URLs when using
+	| the Artisan command line tool. You should set this to the root of
+	| your application so that it is used when running Artisan tasks.
 	|
 	*/
 
-	'model' => 'App\User',
+	'url' => 'http://localhost',
 
 	/*
 	|--------------------------------------------------------------------------
-	| Authentication Table
+	| Application Timezone
 	|--------------------------------------------------------------------------
 	|
-	| When using the "Database" authentication driver, we need to know which
-	| table should be used to retrieve your users. We have chosen a basic
-	| default value but you may easily change it to any table you like.
+	| Here you may specify the default timezone for your application, which
+	| will be used by the PHP date and date-time functions. We have gone
+	| ahead and set this to a sensible default for you out of the box.
 	|
 	*/
 
-	'table' => 'users',
+	'timezone' => 'Asia/Kolkata',
 
 	/*
 	|--------------------------------------------------------------------------
-	| Password Reset Settings
+	| Application Locale Configuration
 	|--------------------------------------------------------------------------
 	|
-	| Here you may set the options for resetting passwords including the view
-	| that is your password reset e-mail. You can also set the name of the
-	| table that maintains all of the reset tokens for your application.
-	|
-	| The expire time is the number of minutes that the reset token should be
-	| considered valid. This security feature keeps tokens short-lived so
-	| they have less time to be guessed. You may change this as needed.
+	| The application locale determines the default locale that will be used
+	| by the translation service provider. You are free to set this value
+	| to any of the locales which will be supported by the application.
 	|
 	*/
 
-	'password' => [
-		'email' => 'emails.password',
-		'table' => 'password_resets',
-		'expire' => 60,
+	'locale' => 'en',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Fallback Locale
+	|--------------------------------------------------------------------------
+	|
+	| The fallback locale determines the locale to use when the current one
+	| is not available. You may change the value to correspond to any of
+	| the language folders that are provided through your application.
+	|
+	*/
+
+	'fallback_locale' => 'en',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Encryption Key
+	|--------------------------------------------------------------------------
+	|
+	| This key is used by the Illuminate encrypter service and should be set
+	| to a random, 32 character string, otherwise these encrypted strings
+	| will not be safe. Please do this before deploying an application!
+	|
+	*/
+
+	'key' => env('APP_KEY', 'jotter)(*&^%$#@!rettoj'),
+
+	'cipher' => MCRYPT_RIJNDAEL_128,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Logging Configuration
+	|--------------------------------------------------------------------------
+	|
+	| Here you may configure the log settings for your application. Out of
+	| the box, Laravel uses the Monolog PHP logging library. This gives
+	| you a variety of powerful log handlers / formatters to utilize.
+	|
+	| Available Settings: "single", "daily", "syslog", "errorlog"
+	|
+	*/
+
+	'log' => 'daily',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Autoloaded Service Providers
+	|--------------------------------------------------------------------------
+	|
+	| The service providers listed here will be automatically loaded on the
+	| request to your application. Feel free to add your own services to
+	| this array to grant expanded functionality to your applications.
+	|
+	*/
+
+	'providers' => [
+
+		/*
+		 * Laravel Framework Service Providers...
+		 */
+		'Illuminate\Foundation\Providers\ArtisanServiceProvider',
+		'Illuminate\Auth\AuthServiceProvider',
+		'Illuminate\Bus\BusServiceProvider',
+		'Illuminate\Cache\CacheServiceProvider',
+		'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
+		'Illuminate\Routing\ControllerServiceProvider',
+		'Illuminate\Cookie\CookieServiceProvider',
+		'Illuminate\Database\DatabaseServiceProvider',
+		'Illuminate\Encryption\EncryptionServiceProvider',
+		'Illuminate\Filesystem\FilesystemServiceProvider',
+		'Illuminate\Foundation\Providers\FoundationServiceProvider',
+		'Illuminate\Hashing\HashServiceProvider',
+		'Illuminate\Mail\MailServiceProvider',
+		'Illuminate\Pagination\PaginationServiceProvider',
+		'Illuminate\Pipeline\PipelineServiceProvider',
+		'Illuminate\Queue\QueueServiceProvider',
+		'Illuminate\Redis\RedisServiceProvider',
+		'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
+		'Illuminate\Session\SessionServiceProvider',
+		'Illuminate\Translation\TranslationServiceProvider',
+		'Illuminate\Validation\ValidationServiceProvider',
+		'Illuminate\View\ViewServiceProvider',
+		'Regulus\ActivityLog\ActivityLogServiceProvider',
+
+		/*
+		 * Application Service Providers...
+		 */
+		'App\Providers\AppServiceProvider',
+		'App\Providers\BusServiceProvider',
+		'App\Providers\ConfigServiceProvider',
+		'App\Providers\EventServiceProvider',
+		'App\Providers\RouteServiceProvider',
+		/*form , html */
+		'Illuminate\Html\HtmlServiceProvider',
+
+	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Class Aliases
+	|--------------------------------------------------------------------------
+	|
+	| This array of class aliases will be registered when this application
+	| is started. However, feel free to register as many as you wish as
+	| the aliases are "lazy" loaded so they don't hinder performance.
+	|
+	*/
+
+	'aliases' => [
+
+		'App'       => 'Illuminate\Support\Facades\App',
+		'Artisan'   => 'Illuminate\Support\Facades\Artisan',
+		'Auth'      => 'Illuminate\Support\Facades\Auth',
+		'Blade'     => 'Illuminate\Support\Facades\Blade',
+		'Bus'       => 'Illuminate\Support\Facades\Bus',
+		'Cache'     => 'Illuminate\Support\Facades\Cache',
+		'Config'    => 'Illuminate\Support\Facades\Config',
+		'Cookie'    => 'Illuminate\Support\Facades\Cookie',
+		'Crypt'     => 'Illuminate\Support\Facades\Crypt',
+		'DB'        => 'Illuminate\Support\Facades\DB',
+		'Eloquent'  => 'Illuminate\Database\Eloquent\Model',
+		'Event'     => 'Illuminate\Support\Facades\Event',
+		'File'      => 'Illuminate\Support\Facades\File',
+		'Hash'      => 'Illuminate\Support\Facades\Hash',
+		'Input'     => 'Illuminate\Support\Facades\Input',
+		'Inspiring' => 'Illuminate\Foundation\Inspiring',
+		'Lang'      => 'Illuminate\Support\Facades\Lang',
+		'Log'       => 'Illuminate\Support\Facades\Log',
+		'Mail'      => 'Illuminate\Support\Facades\Mail',
+		'Password'  => 'Illuminate\Support\Facades\Password',
+		'Queue'     => 'Illuminate\Support\Facades\Queue',
+		'Redirect'  => 'Illuminate\Support\Facades\Redirect',
+		'Redis'     => 'Illuminate\Support\Facades\Redis',
+		'Request'   => 'Illuminate\Support\Facades\Request',
+		'Response'  => 'Illuminate\Support\Facades\Response',
+		'Route'     => 'Illuminate\Support\Facades\Route',
+		'Schema'    => 'Illuminate\Support\Facades\Schema',
+		'Session'   => 'Illuminate\Support\Facades\Session',
+		'Storage'   => 'Illuminate\Support\Facades\Storage',
+		'URL'       => 'Illuminate\Support\Facades\URL',
+		'Validator' => 'Illuminate\Support\Facades\Validator',
+		'View'      => 'Illuminate\Support\Facades\View',
+		'Form'=> 'Illuminate\Html\FormFacade',
+		'HTML'=> 'Illuminate\Html\HtmlFacade',
+		'Activity' => 'Regulus\ActivityLog\Models\Activity',
+
 	],
 
 ];
