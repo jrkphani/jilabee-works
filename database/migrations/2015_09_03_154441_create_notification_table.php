88827fde24a24900987e2a232c876578f0363045
ManiKandan Ram <manikandan@digitalchakra.in>
2015-09-03 17:07:45 +0530

    notification schema

---
+++ database/migrations/2015_09_03_154441_create_notification_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		 Schema::create('notifications', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('object_id')->unsigned();
+            $table->string('object_type', 128);
+            $table->string('subject', 128)->nullable();
+            $table->text('body')->nullable();
+            $table->boolean('is_read')->default(0);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('notifications');
+	}
+
+}
