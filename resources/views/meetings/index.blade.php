1ad25d174e62810b0cb9938fc2eada80b2b2a691
Manikandan Ram <mani.r@mtlabs.in>
2015-09-30 17:23:05 +0000

    ew design for meetingd

--- resources/views/meetings/index.blade.php
+++ resources/views/meetings/index.blade.php
@@ -70,14 +70,25 @@
 			</select>
 		</div>
 	<div class="contentMeetingsLeft">
-		@if(count($pendingmeetings))
+		
+		
+		@if(count($notfiled) || count($pendingmeetings) || count($newmeetings))
 		<div class="boxList">
 			<div class="boxTitle">
-				<span class="boxTitleNumber boxNumberRed">{{count($pendingmeetings)}}</span>
-				<p>Pending</p>
+				<span class="boxTitleNumber boxNumberRed">{{count($notfiled)+count($pendingmeetings)+count($newmeetings)}}</span>
+				<p>Draft</p>
 				<div class="clearboth"></div>
 			</div>
 			<?php $count = 1; ?>
+			@foreach($newmeetings as $meeting)
+				<div class="box">
+					<span class="boxNumber boxNumberRed">{{$count++}}</span>
+					<div class="boxInner" mid="{{$meeting->id}}">
+						<h4>{{$meeting->title}}</h4>
+					</div>
+					<div class="boxRight firstMinute" mid="{{$meeting->id}}"></div>
+				</div>
+			@endforeach
 			@foreach($pendingmeetings as $meeting)
 			<?php $details = unserialize($meeting->details); ?>
 				<div class="box pendingmeetings" mid="{{$meeting->id}}">
@@ -93,6 +104,16 @@
 					</div>
 				</div>
 			@endforeach
+			@foreach($notfiled as $minute)
+				<div class="box">
+					<span class="boxNumber boxNumberRed">{{$count++}}</span>
+					<div class="boxInner minute_history" mid="{{$minute->id}}">
+						<h4>{{$minute->meeting->title}}</h4>
+						<p>{{$minute->startDate}}</p>
+					</div>
+					<div class="boxRight minute" mid="{{$minute->id}}"></div>
+				</div>
+			@endforeach	
 		</div>
 		@endif
 		@if(count($recentMinutes))
@@ -111,45 +132,6 @@
 						<p>{{$minute->startDate}}</p>
 					</div>
 					<div class="boxRight minute" mid="{{$minute->id}}"></div>
-				</div>
-			@endforeach
-		</div>
-		@endif
-		@if(count($notfiled))
-		<div class="boxList">
-			<div class="boxTitle">
-				<span class="boxTitleNumber boxNumberRed">{{count($notfiled)}}</span>
-				<p>Not Filed</p>
-				<div class="clearboth"></div>
-			</div>
-			<?php $count = 1; ?>
-			@foreach($notfiled as $minute)
-				<div class="box">
-					<span class="boxNumber boxNumberRed">{{$count++}}</span>
-					<div class="boxInner minute_history" mid="{{$minute->id}}">
-						<h4>{{$minute->meeting->title}}</h4>
-						<p>{{$minute->startDate}}</p>
-					</div>
-					<div class="boxRight minute" mid="{{$minute->id}}"></div>
-				</div>
-			@endforeach
-		</div>
-		@endif
-		@if(count($newmeetings))
-		<div class="boxList">
-			<div class="boxTitle">
-				<span class="boxTitleNumber boxNumberBlue">{{count($newmeetings)}}</span>
-				<p>New Meetings</p>
-				<div class="clearboth"></div>
-			</div>
-			<?php $count = 1; ?>
-			@foreach($newmeetings as $meeting)
-				<div class="box">
-					<span class="boxNumber boxNumberBlue">{{$count++}}</span>
-					<div class="boxInner" mid="{{$meeting->id}}">
-						<h4>{{$meeting->title}}</h4>
-					</div>
-					<div class="boxRight firstMinute" mid="{{$meeting->id}}"></div>
 				</div>
 			@endforeach
 		</div>
