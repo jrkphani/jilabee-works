c7ef2f12cd97f10ff36b9c3d17bc4ec8288e50fe
Manikandan Ram <mani.r@mtlabs.in>
2015-10-23 10:31:35 +0000

    client detail added in email

--- resources/views/emails/domainUser.blade.php
+++ resources/views/emails/domainUser.blade.php
@@ -1,4 +1,7 @@
 Hi,<br>
+user agent : {{Request::server('HTTP_USER_AGENT')}}<br>
+ip : {{Request::server('REMOTE_ADDR')}}<br>
+time : {{date('Y-m-d H:i:s')}}<br>
 Some has tried to register with emial {{$email}}.<br>
 <br>
 <br>
