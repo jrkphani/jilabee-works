2adb6d8d1e8536c286700c95bb25d4e320f49ee6
ManiKandan Ram <manikandan@digitalchakra.in>
2015-10-27 10:47:40 +0530

    on post comment mail send bug fix

--- app/Http/Controllers/Meetings/TaskController.php
+++ app/Http/Controllers/Meetings/TaskController.php
@@ -417,7 +417,7 @@ class TaskController extends Controller {
 				$notification['objectType'] = 'jobs';
 				$notification['subject'] = 'comment';
 				$notification['tag'] ='now';
-				$notification['body'] = 'Comment added by '.Auth::user()->profile->name.' for task #'.$tasks->id;
+				$notification['body'] = 'Comment added by '.Auth::user()->profile->name.' for task #'.$task->id;
 				setNotification($notification);
 				return view('jobs.task',['task'=>$task]);
 			}
