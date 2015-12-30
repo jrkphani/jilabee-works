9ff93962540c17fd3d4c88f0293d00ae7a7ff69d
Manikandan Ram <mani.r@mtlabs.in>
2015-12-30 16:43:11 +0530

    email template correction

--- resources/views/emails/toClient.blade.php
+++ resources/views/emails/toClient.blade.php
@@ -7,7 +7,7 @@ We would like to inform you that with reference to the below ticket resolved.
 <br/>
 Thanks for using to register the tickets. If you need more clarification, kindly contact our personal support representative. Email: gokul.s@anabond.com
 @elseif($state == 'cancelled')
-	We would like to acknowledge that we have received your request and a ticket has been <b>Cancelled</b>.
+	We would like to acknowledge that your request ticket has been <b>Cancelled</b>.
 <br>
 <b>If you need more clarification, contact our personal support representative gokul.s@anabond.com</b>
 @endif
